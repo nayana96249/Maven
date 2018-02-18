59496c2a50a038e0f969e27abd9efe279236022d
harshal <harshal@harsh>
2018-02-19 00:33:38 +0530

    <Harshal> Added ExtendReport

---
+++ Maven_TestNG_Jenkins_GitHub/src/test/java/tests/ExtendReport.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ExtendReport {
+  @Test
+  public void f() 
+  {
+	  ExtentReports report = null;
+	  ExtentTest test = null;
+	  
+	  
+	  report = new ExtentReports("./test-output/AvinashReport.html");
+	  test = report.startTest("MavenTest");
+	  test.log(LogStatus.PASS	, "FirstStep");
+	  test.log(LogStatus.PASS	, "SecondStep");
+	  report.endTest(test);
+	  if(report != null)
+	  {
+		  report.flush();
+		  report.close();
+		  report = null;
+	  }
+	  }
+	  
+}
---
+++ Maven_TestNG_Jenkins_GitHub/src/test/java/tests/FirstTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void f() 
+  {
+	  System.out.println("Avinash ");
+	  System.out.println("Avinash ");
+	  System.out.println("Avinash "); //
+	  
+	  
+  }
+}
